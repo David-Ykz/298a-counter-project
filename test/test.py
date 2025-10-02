d3db536562d688383c53a2e7864e987e787247c3
David-Ykz <kzdavid314@gmail.com>
2025-10-01 23:30:24 -0400

    set enable

--- test/test.py
+++ test/test.py
@@ -27,7 +27,8 @@ async def test_project(dut):
 
     # Set the input values you want to test
     dut.ui_in.value = 10
-
+    # set enable
+    dut.ena.value = 1
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
